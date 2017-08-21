1510a4f4309777fbd22b22d4e7243bad538c7daf
xwei <xiawei@huomaotv.com>
2017-08-21 18:22:24 +0800

    url 统一转为小写

--- resty/main.lua
+++ resty/main.lua
@@ -11,7 +11,8 @@ local cache = require "resty.hmcache"
 local cList = require "resty.config"
 local re_find = ngx.re.find
 local is_cache = nil
-local uri = ngx.re.sub(ngx.var.request_uri, "\\?.*", "")
+local request_uri = ngx.var.request_uri
+local uri = ngx.re.sub(request_uri, "\\?.*", "")
 
 for regex, exptime in pairs(cList) do
     local from, to, err = re_find(uri, regex, "ijo")
@@ -32,8 +33,16 @@ end
 
 if ngx.var.request_method == "GET" then
 
+    local cache_keys = {
+        ngx.req.get_method(),
+        ngx.var.host,
+        request_uri,
+    }
+    local cache_key = string.lower(table.concat(cache_keys,"|"))
+    local key = 'ngx_cache_' .. ngx.md5(cache_key)
+
     cache = cache:new()
-    local content, err = cache:get(ngx.var.key)
+    local content, err = cache:get(key)
 
     if not content then 
         ngx.log(ngx.ERR, err)
